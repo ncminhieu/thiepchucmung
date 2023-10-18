c1df13bcc49c13f9258fcc7e4ff0703765ebebd3
ncminhieu <148377966+ncminhieu@users.noreply.github.com>
2023-10-19 01:39:51 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Trang Vuong';
+const giftUrl = 'https://www.facebook.com/profile.php?id=61550618016062&mibextid=LQQJ4d';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng bạn';
+const contentCard = 'Chúc bạn 20/10 tràn ngập niềm vui và những nụ cười.Mong rằng ngày mai bạn sẽ trở thành phiên bản mà mình thích, có tự do và ánh sáng.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anh.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
